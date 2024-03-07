88fee49fbfcc18ef61caa649e756d6f4e277bdda
Dinu <dinutud123@gmail.com>
2024-03-07 12:56:32 +0100

    Hangman day 5

---
+++ day5/main.py
@@ -0,0 +1,55 @@
+import random
+from hangman_words import *
+from hangman_art import *
+import os
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(logo)
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+used_letters = []
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    os.system('cls||clear')
+    if guess in used_letters:
+        print(f"You've already guessed {guess}")
+        print(f"{' '.join(display)}")
+        print(stages[lives])
+        continue
+    else:
+        used_letters.append(guess)
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print(f"You lose. The word was '{chosen_word}'.")
+        else:
+            print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    print(stages[lives])
